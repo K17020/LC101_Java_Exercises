4343950b220763b4a77ab1d086658e44ea1a4f04
Kevin Tam <tam.kevin@hotmail.com>
2017-07-31 17:44:46 -0700

    add the base for MPG calculator

---
+++ src/exercises/MpgCalculator.java
@@ -0,0 +1,10 @@
+package exercises;
+import java.util.Scanner;
+// Calculate the MPG
+public class MpgCalculator {
+    public static void main(String[] args){
+        // MPG = Miles/Gallons
+        System.out.println("How many miles have you driven?");
+
+    }
+}
