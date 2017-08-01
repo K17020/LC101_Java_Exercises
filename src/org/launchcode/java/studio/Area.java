93b46a9dc7d8b8edb7f1f54b3eaf730aa67257d1
Kevin Tam <tam.kevin@hotmail.com>
2017-07-31 19:56:40 -0700

    refactor code for printf and add method for calculation

--- src/org/launchcode/java/studio/Area.java
+++ src/org/launchcode/java/studio/Area.java
@@ -3,27 +3,28 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Area {
-        // Shared scanner for keyboard input
+    // Shared scanner for keyboard input
     private static Scanner scanner = new Scanner(System.in);
-        // Program Starts here to find the area of a circle
+    // Method that performs the calculation
+    public static double calArea(double radius){
+        return (Math.PI * radius * radius);
+    }
     public static void main(String[] args){
         Double radius = 0.0;
         while(radius <= 0) {
             System.out.print("Enter a radius: ");
-            // Assign the input as a string for the user
-            String user_input = scanner.nextLine();
-            // Converts the user input into an double
-            radius = Double.parseDouble(user_input);
+            // Assign the input
+            radius = scanner.nextDouble();
             // Checks to see if the number is entered is a negative number
             if (radius <= 0) {
-                System.out.print("Please enter a positive number.\n");
+                System.out.println("Please enter a positive number.");
             }
-            // If the radius is not negative then keep asking for a positive number
+            // If it's not negative calculate
             else if (radius > 0) {
                 // Does the math for the Area of the circle
-                Double area = Math.PI * radius * radius;
+                Double area = calArea(radius);
                 // print the area of a Circle;
-                System.out.print("The area of a circle of radius " + user_input + " is: " + area);
+                System.out.printf("The area of a circle with a %f is %f", radius, area);
             }
         }
     }
