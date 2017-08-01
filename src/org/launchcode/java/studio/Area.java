c08aad6e9de257d60ae854fd5806746e6765a545
Kevin Tam <tam.kevin@hotmail.com>
2017-07-31 17:11:31 -0700

    Add conditional for asking for a positive number

--- src/org/launchcode/java/studio/Area.java
+++ src/org/launchcode/java/studio/Area.java
@@ -7,20 +7,24 @@ public class Area {
     private static Scanner scanner = new Scanner(System.in);
         // Program Starts here to find the area of a circle
     public static void main(String[] args){
-        System.out.print("Enter a radius: ");
+        Double radius = 0.0;
+        while(radius <= 0) {
+            System.out.print("Enter a radius: ");
             // Assign the input as a string for the user
-        String user_input = scanner.nextLine();
+            String user_input = scanner.nextLine();
             // Converts the user input into an double
-        Double radius = Double.parseDouble(user_input);
+            radius = Double.parseDouble(user_input);
             // Checks to see if the number is entered is a negative number
-        if ( radius <= 0){
-            System.out.print("Please enter a positive number.");
-        }
-        else if (radius > 0){
-            // Does the math for the Area of the circle
-            Double area = Math.PI * radius * radius;
-            // print the area of a Circle;
-            System.out.print(area);
+            if (radius <= 0) {
+                System.out.print("Please enter a positive number.\n");
+            }
+            // If the radius is not negative then keep asking for a positive number
+            else if (radius > 0) {
+                // Does the math for the Area of the circle
+                Double area = Math.PI * radius * radius;
+                // print the area of a Circle;
+                System.out.print(area);
+            }
         }
     }
 }
