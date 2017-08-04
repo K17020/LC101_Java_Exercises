929021a5807409ab0cc42df7eb03927d74b598bd
Kevin Tam <tam.kevin@hotmail.com>
2017-08-03 17:25:27 -0700

    Finish MPG Calculator

--- src/exercises/MpgCalculator.java
+++ src/exercises/MpgCalculator.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 public class MpgCalculator {
     public static void main(String[] args){
         // MPG = Miles/Gallons
+        Scanner user_input = new Scanner(System.in);
         System.out.println("How many miles have you driven?");
+        int miles = user_input.nextInt();
+        System.out.println("How much gas did you use?");
+        int gas = user_input.nextInt();
+        System.out.printf("Your car has an MPG of " + miles/gas);
 
     }
 }
